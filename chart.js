e24cbdc2b6f1faf2accca0118d6887091f2547dd
Petros Skordas <p15skor@ionio.gr>
2018-03-05 22:39:49 +0200

    Update chart.js

--- chart.js
+++ chart.js
@@ -352,6 +352,7 @@ function mouseover(d, i) {
     .style("top", (parseInt(d3.select(this).attr("cy") - (d.radius+150)) + offset.top) + "px")
 		.html(infoBox)
 			.style("display","block");
+	responsiveVoice.speak("Hello World");
 	
 	
 	}
