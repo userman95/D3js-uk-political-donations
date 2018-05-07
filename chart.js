a8a30d5b546f4f650cd62bdd64664fc0e2f90a8a
p15kivr <p15kivr@ionio.gr>
2018-05-07 20:48:06 +0300

    Update chart.js

--- chart.js
+++ chart.js
@@ -344,12 +344,7 @@ function mouseover(d, i) {
     .style("top", (parseInt(d3.select(this).attr("cy") - (d.radius+150)) + offset.top) + "px")
 		.html(infoBox)
 			.style("display","block");
-	
-	var img = document.createElement("img");
-  	img.src = imageFile;
-  	img.width = 42;
-  	img.height = 42;
-	document.body.appendChild(img);
+
 	
 	}
 
