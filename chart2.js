f691599ec97e3732bcdcd9d2f278f53dba0543f8
Orestis Kaplanis <orestismail71@gmail.com>
2018-05-29 23:19:12 +0300

    Update chart2.js

--- chart2.js
+++ chart2.js
@@ -73,7 +73,7 @@ function start() {
 		// though I admit I'm asking a lot of the browser and cpu with the number of nodes
 		//.style("opacity", 0.9)
 		.attr("r", 0)
-		.style("fill", function(d) { return fill(d.group); })
+		.style("fill", function(d) { return fill(d.type); })
 		.on("mouseover", mouseover)
 		.on("mouseout", mouseout)
 	        .on("click", function(d){
