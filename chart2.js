3e0440792b4fd57720b1758d45ece99d262c6ef3
Orestis Kaplanis <orestismail71@gmail.com>
2018-05-29 21:32:08 +0300

    Update chart2.js

--- chart2.js
+++ chart2.js
@@ -254,7 +254,7 @@ function display(data) {
 	data.forEach(function(d, i) {
 		var y = radiusScale(d.amount);
 		var node = {
-				radius: radiusScale(d.amount) / 5,
+				radius: radiusScale(d.amount) / 2,
 				value: d.amount,
 				place: d.location,
 				type: d.type,
