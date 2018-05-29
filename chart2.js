da1f8b3497d8949423e11c4b85bc1486af5b5018
Orestis Kaplanis <orestismail71@gmail.com>
2018-05-29 21:52:42 +0300

    Update chart2.js

--- chart2.js
+++ chart2.js
@@ -7,9 +7,9 @@ var brake = 0.2;
 var radius = d3.scale.sqrt().range([10, 20]);
 
 var groupCentres = { 
-    LRUN25FE: { x: w / 3, y: h / 3.3}, 
-    LRUN25MA: {x: w / 3, y: h / 2.3}, 
-    LRUN25TT: {x: w / 3	, y: h / 1.8}
+    con: { x: w / 3, y: h / 3.3}, 
+    lab: {x: w / 3, y: h / 2.3}, 
+    lib: {x: w / 3	, y: h / 1.8}
   };
 
 
